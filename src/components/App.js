2e5ffddff07364348211bc897e3b037f6003eab2
Seeun Bae <morendo90@gmail.com>
2022-02-15 15:27:06 +0900

    Create Login form

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
-import AppRouter from "./Router";
+import { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
-  return <AppRouter />; 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  return (
+    <AppRouter isLoggedIn={isLoggedIn} />
+  ); 
 }
 
 export default App;
